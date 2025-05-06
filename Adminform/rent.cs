82ebc7c3baf65d040c694816a71697abf4459ce2
Gin Gin C. Publico <152463601+ghinzy01@users.noreply.github.com>
2025-05-06 18:04:25 +0800

    Add files via upload

---
+++ Adminform/rent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BogsyVideoStore1.Adminform
+{
+    public partial class rent : Form
+    {
+        public rent()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
